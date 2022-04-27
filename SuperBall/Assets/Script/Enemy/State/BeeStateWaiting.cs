6db9e90314d8839d1ad0ecfcaa3a83a2848082b9
yuta6686 <yuta20010614@gmail.com>
2022-04-27 18:13:53 +0900

    エフェクト　眼が光るやつ

--- SuperBall/Assets/Script/Enemy/State/BeeStateWaiting.cs
+++ SuperBall/Assets/Script/Enemy/State/BeeStateWaiting.cs
@@ -11,6 +11,9 @@ public partial class EnemyManager
             owner.SetAnimation(owner.idolAnimationName);
             owner.StateTransition(owner.stateIdol, 3);
             owner.rigidbody2D.velocity = Vector2.zero;
+            owner.GetComponentInChildren<ParticleSystem>().GetComponentInChildren<Renderer>().material.SetFloat("Green", 3.0f);
+            owner.GetComponentInChildren<ParticleSystem>().Stop();
+            
         }
 
         public override void OnCollision(EnemyManager owner)
