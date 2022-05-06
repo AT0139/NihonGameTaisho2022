930c88ff30989f498955e6788d1ddbca32d4bcc6
LAPTOP-6IV00NGU\81702 <bass.knms7031@gmail.com>
2022-05-06 17:32:56 +0900

    ジャンプのSEと壊れる壁のSEの追加

--- SuperBall/Assets/Script/Block/DestructibleBlock.cs
+++ SuperBall/Assets/Script/Block/DestructibleBlock.cs
@@ -6,6 +6,17 @@ public class DestructibleBlock : MonoBehaviour
 {
     [SerializeField] GameObject explosion;
     readonly private float PLAYER_MAGNITUDE = 5;
+
+    //サウンド処理
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        //Componentを取得
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.name == "ball 1")
@@ -16,7 +27,9 @@ public class DestructibleBlock : MonoBehaviour
             {
                 Debug.Log("破壊");
                 Destroy(gameObject);
+                AudioSource.PlayClipAtPoint(sound1, transform.position);//効果音をならす
                 Instantiate(/*(GameObject)Resources.Load("Explosion001")*/explosion, transform.position, new Quaternion());
+                
             }
         }
     }
