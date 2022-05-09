6becae98353e2ce3b15d814205d043bbea1f0ee5
yuta6686 <yuta20010614@gmail.com>
2022-05-09 18:17:18 +0900

    ダメージエフェクト（UI)

---
+++ SuperBall/Assets/Script/Effect/S_EffectDamageUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_EffectDamageUI : MonoBehaviour
+{
+    public static S_EffectDamageUI instance;
+
+    public  ParticleSystem landingEffect;
+    private ParticleSystem effect;
+
+    public float posY = -400;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effect = Instantiate(landingEffect,transform.position,Quaternion.identity);
+        effect.Stop();
+        effect.transform.SetParent(transform, false);
+        effect.transform.localPosition = new Vector3(0, posY, 0);
+    }
+
+    public void PlayUIEffects()
+    {
+        effect.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayUIEffects();
+        }
+    }
+}
