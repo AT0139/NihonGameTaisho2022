30b7a4f6b09998385c95b1636e9a937a878a0e1b
kimi <nekosikatyo@icloud.com>
2022-05-09 18:39:47 +0900

    ギミック13と14を追加

---
+++ SuperBall/Assets/Script/Gimmick/Gimick13.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimick13 : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;            //    スプライト変えるため
+   
+    public Sprite buttonSprite; //スプライト
+    public Sprite buttonSprite2; //スプライト
+
+   
+    private bool IsCollision = false;
+
+    // 親に通知のため
+    private Gimick13_StateManager _SwitchManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // 親オブジェクトのスクリプトを取得
+        _SwitchManager = transform.parent.gameObject.GetComponent<Gimick13_StateManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //プレイヤーならスプライト切り替え
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if(IsCollision)
+            {
+                spriteRenderer.sprite = buttonSprite2;
+               
+                IsCollision = false;
+
+                //スイッチオフを親に通知
+                _SwitchManager.sendOFF();
+
+            }
+            else
+            {
+                spriteRenderer.sprite = buttonSprite;
+
+                IsCollision = true;
+
+                //スイッチオンを親に通知
+                _SwitchManager.sendON();
+            }
+           
+        }
+    }
+}
---
+++ SuperBall/Assets/Script/Gimmick/Gimick14.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimick14 : MonoBehaviour
+{
+    //このオブジェクトの開始時のサイズを格納する
+    private Vector3 InitScale;
+
+    //現在消えているか否か
+    public bool IsClean = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitScale = transform.localScale;
+
+        //消えているかどうかの初期化
+        if (IsClean)
+        {
+            transform.localScale = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            transform.localScale = new Vector3(InitScale.x, InitScale.y, InitScale.z);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    //大きさを変えて出したり消したりします
+    public void ChengeScale()
+    {
+        if (IsClean)
+        {
+            transform.localScale = new Vector3(InitScale.x, InitScale.y, InitScale.z);
+            IsClean = false;
+        }
+        else
+        {
+            transform.localScale = new Vector3(0, 0, 0);
+            IsClean = true;
+        }
+    }
+}
