716d9a43a8b5ee5586aaeed8a58228d7e965dae0
yuta6686 <yuta20010614@gmail.com>
2022-05-13 17:49:42 +0900

    GameScene_01,_02,03を削除。 GameScene01,02,03（_が無いVer）を追加。 シーン遷移修正。 SceneObjectのMainCameraとFadeCanvasは子供オブジェクトにしては行けない。

---
+++ SuperBall/Assets/Script/SceneTransition/ToGameSelectStage_Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToGameSelectStage_Player : MonoBehaviour
+{
+    GameObject sceneControllerCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneControllerCam = Camera.main.gameObject;
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+
+    void ChangeStageSelectScene()
+    {
+        sceneControllerCam.GetComponent<SceneController>().sceneChange("StageSelectScene");
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.name == "GameStart")
+        {
+            Invoke("ChangeStageSelectScene", 3);
+        }
+
+        if (collision.transform.name == "Exit")
+        {
+
+            Invoke("Quit", 3);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
