f1ae706838974c519f2791d9bfcc1c7e694f4b4a
古市 <galeos7@yahoo.co.jp>
2022-05-13 15:33:54 +0900

    UI

---
+++ SuperBall/Assets/Script/UI/ScoreChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreChange : MonoBehaviour
+{
+    [SerializeField]
+    //private Rawimage ScoreDigit_2; // ２桁目
+
+    //[SerializeField]
+    private GameObject ScoreDigit_1; // １桁目
+
+    private int m_Score;
+
+    void Start()
+    {
+        m_Score = 0;
+
+        ShowScore();
+    }
+
+    private void ShowScore()
+    {
+        
+    }
+
+    public void SetScore(int score)
+    {
+        m_Score += score;
+    }
+}
