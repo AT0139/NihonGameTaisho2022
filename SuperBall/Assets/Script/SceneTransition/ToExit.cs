061236b1d6a439ac8647badf53d03e220725a2e2
yuta6686 <yuta20010614@gmail.com>
2022-05-14 12:51:57 +0900

    タイトルシーン作成中

--- SuperBall/Assets/Script/SceneTransition/ToExit.cs
+++ SuperBall/Assets/Script/SceneTransition/ToExit.cs
@@ -2,51 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToGameSelectStage_Player : MonoBehaviour
+public class ToExit : MonoBehaviour
 {
     GameObject sceneControllerCam;
 
-    static public bool IsGameStart = false;
-    static public bool IsExit = false;
+    GameObject player;          
 
-    private float yMove = 0.0f;
-    private float sinValue = 0.0f;        
+    public float TransitionTime = 4.0f;
 
-    public float waveSpeed = 0.01f;
-    public float waveHeight = 200.0f;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         sceneControllerCam = Camera.main.gameObject;
+        player = GameObject.Find("Player");           
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
-       
+           
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.name == "GameStart")
+        if(collision.transform.tag == "Player")
         {
-            IsGameStart = true;
-            //Invoke("ChangeStageSelectScene", 3);
-            
-        }
-
-        if (collision.transform.name == "Exit")
-        {
-            IsExit = true;
-            //Invoke("Quit", 3);
+            animator.SetBool("blMove", true);
+            Invoke("Quit", TransitionTime);
         }
     }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Invoke("AnimatorSetBoolFalse", 1.0f);
+    }
 
-
-
+    void AnimatorSetBoolFalse()
+    {
+        animator.SetBool("blMove", false);
+    }
 
 
 
---
+++ SuperBall/Assets/Script/SceneTransition/ToGameStart.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToGameStart : MonoBehaviour
+{
+    GameObject sceneControllerCam;    
+    
+    public float TransitionTime = 2.0f;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneControllerCam = Camera.main.gameObject;
+    
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            animator.SetBool("blMove", true);
+            Invoke("ChangeStageSelectScene", TransitionTime);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Invoke("AnimatorSetBoolFalse", 1.0f);
+    }
+
+    void AnimatorSetBoolFalse()
+    {
+        animator.SetBool("blMove", false);
+    }
+
+
+    void ChangeStageSelectScene()
+    {
+        sceneControllerCam.GetComponent<SceneController>().sceneChange("StageSelectScene");
+    }
+}
