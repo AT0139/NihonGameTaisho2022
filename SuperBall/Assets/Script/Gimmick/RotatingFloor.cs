5398e4efb0276113aea0793b71e378681068a882
qd4ws3589@outlook.jp <qdwaa5@gmail.com>
2022-05-15 20:17:12 +0900

    回る床。　背景途中。　

---
+++ SuperBall/Assets/Script/Gimmick/RotatingFloor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingFloor : MonoBehaviour
+{
+    [SerializeField]
+    float angle = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, angle));
+    }
+}
--- SuperBall/Assets/Script/Player/PlayerBGManager.cs
+++ SuperBall/Assets/Script/Player/PlayerBGManager.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class PlayerBGManager : MonoBehaviour
 {
     GameObject parallaxBG;
+    ParallaxBackground pb;
     // Start is called before the first frame update
     void Start()
     {
+        parallaxBG = GameObject.FindGameObjectWithTag("Background_Parallax");
+        if (!parallaxBG)
+            Debug.Log("ヒエラルキーに背景がありません。");
+        if(parallaxBG)
+            pb = parallaxBG.GetComponent<ParallaxBackground>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(parallaxBG)
-            parallaxBG.GetComponent<ParallaxBackground>().StartScroll(transform.position);
+            pb.StartScroll(transform.position);
     }
 }
