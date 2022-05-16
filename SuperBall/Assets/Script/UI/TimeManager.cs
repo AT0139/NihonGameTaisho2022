75537b8949ff1cba6e5a25f330fae5842b6e5bc6
古市 <galeos7@yahoo.co.jp>
2022-05-16 19:27:55 +0900

    タイム

---
+++ SuperBall/Assets/Script/UI/TimeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField]
+    private TimeChange timeChange;
+
+    [SerializeField]
+    private int m_Time;
+
+    private float seconds;
+
+    void Start()
+    {
+        timeChange.ShowTime(m_Time);
+    }
+
+    void Update()
+    {
+        seconds += Time.deltaTime;
+
+        if (seconds >= 1)
+        {
+            seconds = 0;
+
+            m_Time--;
+
+            timeChange.ShowTime(m_Time);
+        }
+
+        if(m_Time < 0)
+        {
+            // ゲームオーバー
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+
+    public int GetTime()
+    {
+        return m_Time;
+    }
+}
