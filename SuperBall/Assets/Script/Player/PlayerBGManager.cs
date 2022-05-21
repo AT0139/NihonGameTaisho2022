71bc67637d7dfaecf788f6dca646660336925004
qd4ws3589@outlook.jp <qdwaa5@gmail.com>
2022-05-21 23:01:23 +0900

    付いてなかったのでプレイヤーにPlayerBGManagerを追加

--- SuperBall/Assets/Script/Player/PlayerBGManager.cs
+++ SuperBall/Assets/Script/Player/PlayerBGManager.cs
@@ -19,6 +19,7 @@ public class PlayerBGManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(pb != null)
             pb.StartScroll(transform.position);
     }
 }
