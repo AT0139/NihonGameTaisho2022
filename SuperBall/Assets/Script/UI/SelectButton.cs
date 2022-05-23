520becc6e1eb4cf56479eb745817b63b2dc3f940
yuta6686 <yuta20010614@gmail.com>
2022-05-23 13:52:28 +0900

    AreaSelect見た目変更

---
+++ SuperBall/Assets/Script/UI/SelectButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SelectButton : MonoBehaviour
+{
+    private Image image;
+    private Animator animator;
+
+
+    public void ChangeSprite(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+
+    public void ChangeMaterial(Material material)
+    {
+        image.material = material;
+    }
+
+
+    public void SelectBehavior()
+    {
+        animator.SetBool("IsSelect", true);
+        image.material.SetFloat("_IsBlinking", 0);
+    }
+
+    public void DeselectBehavior()
+    {
+        animator.SetBool("IsSelect", false);
+        image.material.SetFloat("_IsBlinking", 1);
+    }
+
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
