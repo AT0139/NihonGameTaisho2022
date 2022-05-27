e79d3f8ad948b5037136a4631b8e1e53b85e200e
AT0139 <takumi.study@outlook.jp>
2022-05-27 16:44:56 +0900

    ハリネズミ追加

--- SuperBall/Assets/LayerName.cs
+++ SuperBall/Assets/LayerName.cs
@@ -9,10 +9,12 @@ public static class LayerName
 	public const int Water = 4;
 	public const int UI = 5;
 	public const int Ground = 8;
+	public const int Tilemap = 9;
 	public const int DefaultMask = 1;
 	public const int TransparentFXMask = 2;
 	public const int IgnoreRaycastMask = 4;
 	public const int WaterMask = 16;
 	public const int UIMask = 32;
 	public const int GroundMask = 256;
+	public const int TilemapMask = 512;
 }
--- SuperBall/Assets/Script/Enemy/State/Worm/WormStateAttack.cs
+++ SuperBall/Assets/Script/Enemy/State/Worm/WormStateAttack.cs
@@ -9,26 +9,32 @@ public partial class EnemyManager
     {
         int ATTACK_TIME = 60;
         int atkCnt = 0;
+        EnemyManager m_owner;
 
         public override void OnEnter(EnemyManager owner)
         {
-            
+            m_owner = owner;
+            Spine.TrackEntry trackEntry = owner.SetAnimation(owner.attackAnimationName);
+            trackEntry.Complete += TrackEntryComplete;
+            //owner.GetComponentInChildren<ParticleSystem>().Play();
+            //Debug.Log(owner.transform.eulerAngles.z);
+            //owner.transform.GetChild(0).GetChild(0).GetComponent<ParticleSystem>().startRotation
+            //    = owner.transform.eulerAngles.z;
         }
 
         public override void OnUpdate(EnemyManager owner)
         {
             owner.rigidbody2D.velocity = new Vector2(0.0f, 0.0f);
-            atkCnt++;
-            if(atkCnt >= ATTACK_TIME)
-            {
-                atkCnt = 0;
-                owner.StateTransition(owner.stateIdol);
-            }
         }
 
         public override void OnCollision(EnemyManager owner)
         {
             SceneManager.LoadScene("GameOverScene");
+        }
+
+        private void TrackEntryComplete(Spine.TrackEntry trackEntry)
+        {
+            m_owner.StateTransition(m_owner.stateIdol);
         }
     }
 }
