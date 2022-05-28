ea0c6dc0a98e884325ad464bc425ed0a31988c9f
qd4ws3589@outlook.jp <qdwaa5@gmail.com>
2022-05-29 01:31:11 +0900

    蜘蛛8割できた

---
+++ SuperBall/Assets/Script/Enemy/State/Spider/Attack_SpiderColonel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_SpiderColonel : IState<Spider>
+{
+    public void Initialize(Spider owner)
+    {
+        owner.enemyAnimeState.SetAnimation(0, owner.ATTACK_ANIMATION, true);
+    }
+
+    public void Execute(Spider owner)
+    {
+        owner.player.GetComponent<PlayerLife>().GetDamege(1);
+        owner.m_StateContext.ChangeState(new Chase_SpiderColonel());
+    }
+
+    public void Terminate(Spider owner)
+    {
+    }
+}
--- SuperBall/Assets/Script/Enemy/State/Spider/ReturnToInitialBase_SpiderColonel.cs
+++ SuperBall/Assets/Script/Enemy/State/Spider/ReturnToInitialBase_SpiderColonel.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class ReturnToInitialBase_SpiderColonel : IState<Spider>
 {
+    Vector3 previousPosition;
     public void Initialize(Spider owner)
     {
-        Debug.Log("initialize return to base");
+        owner.enemyAnimeState.SetAnimation(0, owner.MOVE_ANIMATION, true);
     }
 
     public void Execute(Spider owner)
     {
-       // if(owner.transform.position != initialPosition)
+        Transform children = owner.spiderNetParent.GetComponentInChildren<Transform>();
+        if (children.childCount == 0)
+            return;
+        foreach (Transform nets in children)
+            if (nets.GetComponent<SpiderNet>().playerOnNet)
+            {
+                owner.m_StateContext.ChangeState(new Chase_SpiderColonel());
+            }
+        // if(owner.transform.position != initialPosition)
+
+        if(Vector3.Distance(owner.initialPosition, owner.transform.position) > 0.1f)
+        {
             owner.transform.position = Vector3.MoveTowards(
-                                                owner.transform.position,
-                                                owner.initialPosition,
-                                                owner.getSpeed);
+                                                    owner.transform.position,
+                                                    owner.initialPosition,
+                                                    owner.getSpeed);
+        }
+        else
+        {
+            owner.m_StateContext.ChangeState(new Sleep_SpiderColonel());
+        }
+
+        if (!owner.IsOnNet(owner.transform.position))
+        {
+            owner.transform.position = previousPosition;
+        }
+
+        previousPosition = owner.transform.position;
     }
 
     public void Terminate(Spider owner)
