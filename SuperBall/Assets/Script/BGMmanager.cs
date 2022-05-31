4b857f9ca293e37d563b21afd17a26743a547ae3
古市 <galeos7@yahoo.co.jp>
2022-05-31 17:45:28 +0900

    BGM追加

---
+++ SuperBall/Assets/Script/BGMmanager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMmanager : MonoBehaviour
+{
+    // 登録するBGM
+    [SerializeField] AudioSource Stageselect;
+
+    void Start()
+    {
+        int numMusicPlayers = GameObject.FindGameObjectsWithTag("BGM").Length;
+        // シーン内に他のBGMオブジェクトがあったらtrue
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+
+            Stageselect.Play();
+
+            //シーンが切り替わった時に呼ばれるメソッドを登録
+            SceneManager.activeSceneChanged += OnActiveSceneChanged;
+        }
+    }
+
+    //シーンが切り替わった時に呼ばれるメソッド　
+    void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+    {
+        // 切り替え後selectシーンじゃなかったら prevSceneはnullが入るそうな、どういうことなの…
+        if (!nextScene.name.Contains("Select"))
+        {
+            // 破壊してステージBGMなどの邪魔をしないように
+            Stageselect = null; // 入れないとunityに怒られる?
+            Destroy(gameObject);
+        }
+    }
+}
